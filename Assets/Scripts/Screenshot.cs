ab1061b668b0634fb27071fea2aed4a42c42eaa8
andromikulic@gmail.com <andromikulic@gmail.com>
2023-09-05 18:34:16 +0200

    Add jslib for webgl screenshots.

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Screenshot : MonoBehaviour
 {
+    [DllImport("__Internal")]
+    private static extern void SaveScreenshotWebGL(string filename, string data);
     public void TakeScreenshot()
     {
         var timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss", System.Globalization.CultureInfo.InvariantCulture);
-        ScreenCapture.CaptureScreenshot($"{timestamp}-{QueryParameters.values["modelId"]}.png");
+        var filename = $"{timestamp}-{QueryParameters.values["modelId"]}.png";
+        var data = ScreenCapture.CaptureScreenshotAsTexture().EncodeToPNG().ToString();
+        SaveScreenshotWebGL(filename, data);
     }
 }
 
